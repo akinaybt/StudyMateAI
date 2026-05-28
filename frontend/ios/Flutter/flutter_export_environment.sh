349245e2a6f922dc2737f0dd4e32a46528ce790e
grishkosofi <grishkosofi@gmail.com>
2026-05-28 12:38:58 +0200

    working version

--- frontend/ios/Flutter/flutter_export_environment.sh
+++ frontend/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\SofiiaGrishko\StudioProjects\mate1"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Users\SofiiaGrishko\StudioProjects\mate1\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "FLUTTER_APPLICATION_PATH=C:\Users\SofiiaGrishko\StudioProjects\StudyMateAI\frontend"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Users\SofiiaGrishko\StudioProjects\StudyMateAI\frontend\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
